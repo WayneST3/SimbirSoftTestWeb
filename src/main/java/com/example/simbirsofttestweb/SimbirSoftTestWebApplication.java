f0ee5322b46edc11fcf6f97c48766d72cbf715d9
WayneST3 <luck99bahek@mail.ru>
2022-03-01 15:39:46 +0300

    Project created

---
+++ src/main/java/com/example/simbirsofttestweb/SimbirSoftTestWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.simbirsofttestweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirSoftTestWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbirSoftTestWebApplication.class, args);
+    }
+
+}
